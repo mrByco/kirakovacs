70cbb9c7808993b290595665dfb72f62d28fb55f
matyas <matyi0918@gmail.com>
2024-09-23 07:00:25 +0200

    Maybe keycloack library

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,33 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+    })
+    .AddOpenIdConnect(options =>
+    {
+        options.Authority = "https://keycloack.bitberry.tech/auth/realms/kirakovacs.com";
+        options.ClientId = "backend";
+        options.ClientSecret = "5m3q1Ws6L5pkUXH1ix8Svt9yOFskYszt";
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.CallbackPath = "/login-callback"; // Update callback path
+        options.SignedOutCallbackPath = "/logout-callback"; // Update signout callback path
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "preferred_username",
+            RoleClaimType = "roles"
+        };
+    });
 
 var app = builder.Build();
 
@@ -18,6 +49,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
